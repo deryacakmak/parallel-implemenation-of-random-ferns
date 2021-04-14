6987fff61325d4fa74427eab8cd4ada1fc5fe416
deryacakmak <97deryacakmak@gmail.com>
2021-04-14 16:57:40 +0300

    already done

--- trainingFern/trainingFerns.py
+++ trainingFern/trainingFerns.py
@@ -5,10 +5,7 @@ import math
 from skimage.util import random_noise
 from affineDeformation import applyAffineDeformation
 
-# key: class num 
-# value: (0...n, P(F|C)) 
 allClasses = dict()
-
 PATCH_WIDTH = 32
 NUMBER_OF_FEATURE_EVALUATED_PER_PATCH = 11
 FERN_NUMBER = 11
