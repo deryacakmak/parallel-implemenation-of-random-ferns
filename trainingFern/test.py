144c7dd5a360644b0d3c3f1b9f20ed17d6c39fce
deryacakmak <97deryacakmak@gmail.com>
2021-04-26 02:18:48 +0300

    test continues

--- trainingFern/test.py
+++ trainingFern/test.py
@@ -10,15 +10,24 @@ def hconcat_resize_min(im_list, interpolation=cv2.INTER_CUBIC):
 
 
 
-def drawLine(keypointListOriginal, keypointListMatch, width, img_tmp):
-    for i in range(len(keypointListMatch)):
-        x1, y1 = keypointListMatch[i][0][0], keypointListMatch[i][0][1]+width
-        classNum = keypointListMatch[1][1][1]
-        x2, y2 = keypointListOriginal[classNum][0], keypointListOriginal[classNum][1]
-        cv2.line(img_tmp, (x2, y2), (x1, y1), (0, 255, 0), thickness=2)
-
+def drawLine(matchResult, width, img_tmp):
+    
+    for i in matchResult:
+        x1,y1 = i[1][1]
+        x2,y2 = i[0][0], i[0][1] + width
+        cv2.line(img_tmp, (y2, x2), (y1, x1), (0, 255, 0), thickness=2)
     cv2.imwrite("result.png",img_tmp)
     
 
 matchResult = classifyKeypoint("rotatedImage.png","eiffel_tower.png")
 
+img = cv2.imread("concatanate.png")
+img2 = cv2.imread("eiffel_tower.png")
+
+# for i in matchResult:
+#     x,y = i[1][1]
+#     img[x][y] = 255
+
+# cv2.imwrite("result.png",img)
+
+drawLine(matchResult, img2.shape[:2][1], img)
