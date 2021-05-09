0aef00a895bd819ac76a08a02ea62ef5b4493e8e
deryacakmak <97deryacakmak@gmail.com>
2021-05-10 01:14:31 +0300

    training continue

--- trainingFern/Ferns.py
+++ trainingFern/Ferns.py
@@ -2,13 +2,7 @@ import math
 import cv2
 import trainingFerns as tf
 
-
-
-PATCH_WIDTH = 32
-NUMBER_OF_FEATURE_EVALUATED_PER_PATCH = 11
-FERN_NUMBER = 40
-
-    
+FERN_NUMBER = 50
 
 def classifyKeypoint(imageName, originalImage):
     
@@ -18,35 +12,34 @@ def classifyKeypoint(imageName, originalImage):
     img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     
     
-    keypoints = tf.detectKeypoint(img)
+    keypoints = tf.detectKeypoint(img)[:100]
     
-    trainingClasses, originalImageKeypoints = tf.trainingFerns(originalImage)
+    trainingClasses, originalImageKeypoints, indexList = tf.trainingFerns(originalImage)
 
     match = []
-    
+
     for keypoint in keypoints:
         
         results = [0] * len(originalImageKeypoints)
         
         patch = tf.findPatch(keypoint[0],keypoint[1], img)
         
-        
         for i in range(FERN_NUMBER):
             
-            fern = tf.extractFeature(patch.flatten())
-
-            decimalNum =  int("".join(str(x) for x in fern), 2) 
+            index = indexList[i]
             
+            decimalNum = tf.extractFeature(patch.flatten(), index)
+
             for classNum in range(len(originalImageKeypoints)):
                 
                 classCount = trainingClasses[classNum]
-                
+
                 if(decimalNum in classCount):
                       results[classNum] = math.log(classCount[decimalNum]) + results[classNum] 
                      
-                else:
-                    results[classNum] = math.log(classCount[-1]) + results[classNum] 
-                    
+                # else:
+                #     results[classNum] = math.log(classCount[-1]) + results[classNum] 
+      
         max_value = max(results)
         max_index = results.index(max_value)
         
